ade630aa2cbd40dc7ba2d7a8e2ed39385baef6ff
Gabriel <128435325+gabrielgomessdev@users.noreply.github.com>
2026-04-26 18:14:14 -0300

    implementando com sqlite

---
+++ src/pages/Gastos.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import { FlatList, View, StyleSheet } from 'react-native';
+import { List, Text, FAB } from 'react-native-paper';
+
+import Header from '../components/Header';
+import Container from '../components/Container';
+import Body from '../components/Body';
+
+import {getGastos} from '../services/GastosServicesDB';
+
+import {useNavigation} from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
+
+const Gastos = () => {
+
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const [gastos, setGastos] = useState([]);
+
+  useEffect(() => {
+    getGastos().then((dados)=>{
+      setGastos(dados);
+    });
+  },[isFocused]);
+
+  const renderItem = ({ item }) => (
+    <List.Item
+      title={
+        'R$' + item.valor.toFixed(2) + ' (R$' + item.preco.toFixed(2) + ')'
+      }
+      description={item.odometro + ' km'}
+      left={(props) => (
+        <List.Icon
+          {...props}
+          color={item.tipo == 0 ? 'red' : 'green'}
+          icon="gas-station"
+        />
+      )}
+      right={(props) => (
+        <Text {...props} style={{ alignSelf: 'center' }}>
+          {' '}
+          {item.data}{' '}
+        </Text>
+      )}
+      onPress={() => navigation.navigate('Abastecimento', {item})}
+    />
+  );
+
+  return (
+    <Container>
+      <Header title={'Fuel Manager'} />
+      <Body>
+        <FlatList
+          data={gastos}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+        <FAB
+          style={styles.fab}
+          small
+          icon="plus"
+          onPress={() => navigation.navigate('Abastecimento')}
+        />
+      </Body>
+    </Container>
+  );
+};
+
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+export default Gastos;
